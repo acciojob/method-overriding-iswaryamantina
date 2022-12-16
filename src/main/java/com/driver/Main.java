70b1ba5ba549ae0febb426daf14459188cb6c9ec
iswaryamantina <iswarya252897@gmail.com>
2022-12-16 17:40:43 +0530

    assignment completed

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,21 @@
 package com.driver;
-
 public class Main {
-  
+     public static class A{
+        public String meth(){
+            return "Invoking method from class A";
+        }
+    }
+    public static class B extends A {
+        // override method
+
+        @Override
+        public String meth(){
+            return "Method is overridden in Extendend class B";
+        }
+    }
+ public static void main (String[] args){
+         // create object of class B
+     B objb= new B();
+     objb.meth();
+ }
 }
